0d582b8d0479365bc4ca93821d1c1b644b89eb41
Matt <duvallma@oregonstate.edu>
2021-04-19 21:03:23 -0600

    edit boat tests all pass

--- main.py
+++ main.py
@@ -44,9 +44,27 @@ def boats_get_post():
     else:
         return 'Method not recognized'
 
-@app.route('/boats/<id>', methods=['PUT','DELETE','GET'])
-def boats_put_delete(id):
-    if request.method == 'PUT':
+@app.route('/boats/<id>', methods=['PUT','PATCH','DELETE','GET'])
+def boats_put_patch_delete(id):
+    if request.method == 'PATCH':
+        content = request.get_json()
+        if ("name" in content and "type" in content and "length" in content):
+            boat_key = client.key(constants.boats, int(id))
+            if (boat_key != None):
+                boat = client.get(key=boat_key)
+                if (boat != None):
+                    boat.update({"name": content["name"], "type": content["type"], "length": content["length"]})
+                    client.put(boat)
+                    boat["id"] = boat.key.id
+                    boat["self"] = str(request.url_root) + 'boats/' + str(boat.key.id)
+                    return (json.dumps(boat), 200)
+                else:
+                    return (json.dumps({"Error": "No boat with this boat_id exists"}), 404)
+            else:
+                return (json.dumps({"Error": "No boat with this boat_id exists"}), 404)
+        else:
+            return (json.dumps({"Error": "The request object is missing at least one of the required attributes"}), 400)
+    elif request.method == 'PUT':
         content = request.get_json()
         boat_key = client.key(constants.boats, int(id))
         boat = client.get(key=boat_key)
